aae0606b0220372b868164feaff1cde9a58effff
zhengjintao <xiaonei0912@qq.com>
2018-01-14 02:50:34 +0000

    Update install_IKAnalyzer.bat

--- tools/install_IKAnalyzer.bat
+++ tools/install_IKAnalyzer.bat
@@ -1,2 +1,4 @@
+REM  使用时需安装mvn工具
+REM  也可直接在Eclipse中运行此命令进行注册
 mvn install:install-file -Dfile=IKAnalyzer-2012.jar -DgroupId=org.wltea -DartifactId=IKAnalyzer -Dversion=2012 -DcreateChecksum=true -Dpackaging=jar -DgeneratePom=true
 pause
